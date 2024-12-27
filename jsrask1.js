7ef901873b0f3c4451e7a67ec56ebee692d38eca
ahmma <ahmmadtarawnah@gmail.com>
2024-12-27 14:27:50 +0300

    .

---
+++ jsrask1.js
@@ -0,0 +1,49 @@
+// task 1
+// let x = 12;
+// console.log(- 5 * 3);
+// console.log("JavaScript "+ 50);
+// console.log(17 % 5);
+// console.log(5 % 17);
+// console.log(5 / 10);
+// console.log(4 === '4');
+// console.log(4!=5);
+// console.log(7<=5);
+// console.log("hello"+5);
+// console.log(Math.ceil(x) - Math.floor(x));
+// console.log(Math.pow(x, 2));
+
+
+// task 2
+// let number = prompt("Enter Your Number");
+// alert("Your Number Is "+ number);
+
+
+
+// task 3
+// let num1 = prompt("Enter First Number");
+// let num2 = prompt("Enter Second Number");
+// alert(+num1 + +num2);
+
+// task 4
+// let num1 = prompt("Enter the first number:");
+// let num2 =prompt("Enter the second number:");
+// alert("The product is: " + (num1 * num2));
+
+
+
+//  task5
+// let num1 = prompt("enter the first number");
+// let num2 = prompt("enter the seconde number");
+// if(num2!=0){
+//     alert("The result of division is:" + (num1/num2));
+// }  
+// else{
+//     alert("Division by zero is not allowed");
+//     }
+
+
+// task6
+// let num1 = 100;
+// let num2 = 100;
+// let num = num1 + num2;
+// alert(num);
